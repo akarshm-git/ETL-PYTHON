8eac5a969fb567e0bbe74e69bc6aac80c021f4af
akarshm-git <amallampati@scu.edu>
2023-02-20 12:11:28 -0800

    Looping through  obs

--- etldemo.py
+++ etldemo.py
@@ -22,7 +22,36 @@ url = config['CONFIG']['url']
 destServer = config['CONFIG']['server']
 destDatabase = config['CONFIG']['database']
 
-print(url)
-print(startDate)
-print(destServer)
-print(destDatabase)
+#request data from URL
+try:
+    BOCResponse = requests.get(url+startDate)
+except Exception as e:
+    print("could not make request:"+str(e))
+    sys.exit()
+
+BOCDates = []
+BOCRates = []
+
+#check response status and if valid turn raw text to json from response
+if BOCResponse.status_code == 200:
+    BOCRaw = json.loads(BOCResponse.text)
+
+    #extract observation data into column arrays
+    for row in BOCRaw['observations']:
+        # later going to compare data from the dates column in BOS source data with another data source.(they have  to be in same data type for that)
+        BOCDates.append(datetime.datetime.strptime(row['d'],'%Y-%m-%d'))
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
